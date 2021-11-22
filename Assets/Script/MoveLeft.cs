9f3456fdf5a81e828b54a2c20d103db384c9c79f
JoanBarFul <jonbarful@gmail.com>
2021-11-22 13:36:27 +0100

    1

--- Assets/Script/MoveLeft.cs
+++ Assets/Script/MoveLeft.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 10f;
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,16 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRigidbody;
     public float jumpForce = 400f;
-    public float gravityMod = 1.5f;
+    public float gravityMod = 1f;
     public bool isOnGround = true;
     public bool gameOver;
     private Animator playerAnimator;
     private Animator playerdeath;
+    private int randomDeath;
+    public ParticleSystem particlesExplosion;
+    public ParticleSystem particlesRun;
+    private AudioSource playerAudio;
+    public AudioClip jumpClip;
+    public AudioClip explosionClip;
+    private AudioSource cameraAudio;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,32 +28,50 @@ public class PlayerController : MonoBehaviour
         Physics.gravity *= gravityMod;
         playerAnimator = GetComponent<Animator>();
         playerdeath = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        cameraAudio = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+        
     }
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             playerAnimator.SetTrigger("Jump_trig");
             isOnGround = false;
+            particlesRun.Stop();
+            playerAudio.PlayOneShot(jumpClip, 1);
         }
+
 
         
 
     }
     private void OnCollisionEnter(Collision otherCollider)
     {
+        if (!gameOver)
+        { 
         if (otherCollider.gameObject.CompareTag("Ground"))
-        { isOnGround = true; }
+        { isOnGround = true;
+            particlesRun.Play();
+        }
         if (otherCollider.gameObject.CompareTag("Obstacle"))
         {
-            Debug.Log("GAME OVER");
+            //Debug.Log("GAME OVER");
             gameOver = true;
+            randomDeath = Random.Range(1, 3);
             playerdeath.SetBool("Death_b", true);
-        }
+            playerdeath.SetInteger("DeathType_int", randomDeath);
+            particlesExplosion.Play();
+            particlesRun.Stop();
+            playerAudio.PlayOneShot(explosionClip, 1);
+                cameraAudio.Stop();
+            
 
+        }
+        }
     }
 
 }
